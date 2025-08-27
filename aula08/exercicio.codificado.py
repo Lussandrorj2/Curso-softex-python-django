de24583b20e66e84730a167f20cfefc87ee274cb
Lussandrorj2 <167385164+Lussandrorj2@users.noreply.github.com>
2025-08-27 15:32:25 -0300

    modificando arquivo

--- aula08/exercicio.codificado.py
+++
@@ -1,7 +0,0 @@
-#declaração
-frase = input("Digite uma frase: ").lower()
-
-codificada = frase.replace("a","1").replace("e","2").replace("i","3").replace("o","4").replace("u","5")
-decodificada = frase.replace("1","a").replace("2","e").replace("3","i").replace("4","o").replace("5","u")
-
-print(f"Frase codificada → {codificada} \ " ,f"Frase decodificada {decodificada}")

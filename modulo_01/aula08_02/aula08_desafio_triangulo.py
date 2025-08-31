9b52689820c9577c45d3625f33ef3bae8ea1cce6
Lussandro Farias <167385164+Lussandrorj2@users.noreply.github.com>
2025-08-31 08:00:23 +0000

    Desafio do triangulo

---
+++ modulo_01/aula08_02/aula08_desafio_triangulo.py
@@ -0,0 +1,37 @@
+#Declaração
+print(" ▲ ▲ ▲ DESAFIO DO TRIÂNGULO ▲ ▲ ▲ ")
+print(" ▲ ".center(32))
+print(" ▲▲▲▲ ".center(32))
+print(" ▲▲▲▲▲▲▲▲▲▲ ".center(32))
+print(" ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ ".center(32))
+print(" ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ ".center(32))
+print("")
+lados = []
+while len(lados) < 3:
+    try:
+        lado = int(input(f"\033[034mDigite o {len(lados)+1}º lado do triângulo: \033[m"))
+        if lado > 0:
+            lados.append(lado)
+        else:
+            print("Digite um número positivo e maior que zero.")
+    except ValueError:
+        print("Error: Digite um número inteiro.")
+   
+lado_a, lado_b, lado_c = lados
+
+if (lado_a < lado_b + lado_c) and (lado_b < lado_a + lado_c) and (lado_c < lado_a + lado_b):
+    if (lado_a > abs(lado_b - lado_c)) and (lado_b > abs(lado_a - lado_c)) and (lado_c > abs(lado_a - lado_b)):
+        if lado_a == lado_b and lado_b == lado_c:
+            print("Pode ser um triângulo equilátero.")
+            print(f"Lados dos triângulo {lados}")
+        elif lado_a == lado_b or lado_b == lado_c or lado_a == lado_c:
+            print("Pode ser um triângulo isósceles..")
+            print(f"Lados do triângulo {lados}.")
+        else:
+            print("Pode ser um escaleno.")
+            print(f"Lados do triângulo {lados}")
+    else:
+        print("Não pode ser um triângulo devido a diferença de lados.")
+else:
+    print("Não pode ser um triãngulo devido a soma de lados.")
+

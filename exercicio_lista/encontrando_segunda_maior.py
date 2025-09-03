13215dfe59b5fb03fcdea217c628b66d3e19d498
Lussandro <lussandrorj2@hotmail.com>
2025-09-03 10:03:08 -0300

    exercicio encontrando segundo maior número

--- exercicio_lista/encontrando_segunda_maior.py
+++ exercicio_lista/encontrando_segunda_maior.py
@@ -1,6 +1,6 @@
 numeros = [10, 5, 20, 15]
 
-if len(numeros) < 2:
+if len(numeros) < 2: # Verifica se a lista tem pelo menos 2 elementos
     print("A lista deve ter pelo menos 2 elementos.")
 else:
 
@@ -10,8 +10,7 @@ else:
     else:
         maior = numeros[1]
         segundo_maior = numeros[0]
-
-    for num in numeros[2:]:
+    for num in numeros[2:]: # Inicia o loop a partir 3 item
         if num > maior:
             segundo_maior = maior
             maior = num

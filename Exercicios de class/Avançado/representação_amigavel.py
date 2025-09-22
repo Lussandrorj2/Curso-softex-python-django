b2b0a98de3a1544c2718b24181e692401a7f2e72
Lussandrorj2 <167385164+Lussandrorj2@users.noreply.github.com>
2025-09-22 13:44:33 -0300

    chore: Adicionando nomes de filmes

--- Exercicios de class/Avançado/representação_amigavel.py
+++ Exercicios de class/Avançado/representação_amigavel.py
@@ -5,7 +5,15 @@ class Filme():
         self.ano = ano
 
     def __str__(self):
-        return f"Filme: {self.titulo} ano {self.ano} - Diretor: {self.diretor}"
+        return f" █ Filme: {self.titulo} | Ano: {self.ano} | Diretor: {self.diretor}"
     
-filme1 = Filme("De volta para o futuro", "Robert Zemeckis", 1985)
+filme1 = Filme("De volta para o futuro", "Robert Zemeckis █", 1985)
+filme2 = Filme("Os Goonies", "Richard Donner █", 1985 )
+filme3 = Filme("Matrix", " Lilly Wachowski e Lana Wachowski", 1999)
+print("~"*70)
 print(filme1)
+print("~"*70)
+print(filme2)
+print("~"*70)
+print(filme3)
+print("~"*70)

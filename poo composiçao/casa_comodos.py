31849e2b3f407ddf818b0c6f13d34567e84f3ac0
Lussandrorj2 <167385164+Lussandrorj2@users.noreply.github.com>
2025-09-29 15:46:52 -0300

    feat: class casa com composição comodos

---
+++ poo composiçao/casa_comodos.py
@@ -0,0 +1,12 @@
+class Comodo:
+    def __init__(self, nome: str):
+        self.nome = nome
+
+class Casa:
+    def __init__(self, lista_comodos):
+        self.comodos = []
+
+    def adicionar_comodo(self, nome):
+        self.comodo = Comodo.append("sala")
+    
+    def listar_comodos(self):

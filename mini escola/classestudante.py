77a591b485872c1c66df5fd6e7f5d8d687a3bb51
Lussandrorj2 <167385164+Lussandrorj2@users.noreply.github.com>
2025-10-01 15:07:39 -0300

    feat: class estudante que herda da class pessoa nome e idade e cria dois metodos matricula e nota

---
+++ mini escola/classestudante.py
@@ -0,0 +1,45 @@
+from classpessoa import Pessoa
+
+class Estudante(Pessoa):
+    def __init__(self, nome:str, idade:int, matricula:int):
+        super().__init__(nome, idade)
+        self.matricula = matricula
+        self.notas = {}
+
+    
+
+    def adicionandonotas(self,materia:str, nota:float):
+        if materia in self.notas:
+            self.notas[materia].append(nota)
+        else:
+            self.notas[materia] = [nota]
+
+        print(f"Notas de {materia}: {self.notas[materia]}")
+
+
+    def __str__(self):
+        return f"Estudante: {self._nome} - Matricula: {self.matricula}"
+
+
+estudante = Estudante("João", 20, 1234)
+estudante.adicionandonotas("Matematica", 9.5)
+print(estudante)
+
+print()
+
+estudante2 = Estudante("Maria",19,1235)
+estudante.adicionandonotas("Matemática", 8.7)
+print(estudante2)
+        
+
+
+       
+
+           
+
+
+
+
+
+
+       

37273c1065da645d3f398e542dd68996432fef20
Lussandrorj2 <167385164+Lussandrorj2@users.noreply.github.com>
2025-10-01 15:48:15 -0300

    chore: correção do codigo.

--- mini escola/escola.py
+++ mini escola/escola.py
@@ -2,14 +2,19 @@ from classestudante import Estudante
 
 class Escola:
     def __init__(self):
-        self.lista_estudante = []
+        self.lista_estudante:list[Estudante] = []
 
     def adicionar_estudante(self, estudante):
         self.lista_estudante.append(estudante)
 
     def mostrar_lista(self):
         for i in self.lista_estudante:
-            print(f"Nome: {i.nome} - Matricula: {i.matricula} - Nota: {i.nota}")
+            print(f"Nome: {i.nome} - Matricula: {i.matricula} - Nota: {i.notas}")
+
+
+estudante = Estudante("João", 20, 1234)
+estudante.adicionandonotas("Matematica", 9.5)
 
 escola = Escola()
+escola.adicionar_estudante(estudante)
 escola.mostrar_lista()
